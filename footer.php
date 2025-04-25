987f82cb81332f71f003ec060b5c28bf336eb87b
Alex Kirk <akirk@users.noreply.github.com>
2025-04-25 06:44:55 +0200

    Add a footer menu

--- footer.php
+++ footer.php
@@ -14,9 +14,18 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-
-  <div class="row">
-    <div class="col-xs-12">
+	<!-- Footer nav menu -->
+	<div class="row">
+		<div class="col-xs-12">
+			<nav id="footer-navigation" class="site-navigation footer-navigation" role="navigation">
+				<?php
+					wp_nav_menu( array(
+						'theme_location' => 'footer',
+						'menu_id'        => 'footer-menu',
+						'depth'          => 1,
+					) );
+				?>
+			</nav><!-- #site-navigation -->
         
 		<div class="site-info">
             &copy; <?php echo intval( date_i18n(__('Y','slightly')) ) ?> <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a> &bull; <?php 
